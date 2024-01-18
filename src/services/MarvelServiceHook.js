8436bbd17c0dca6f6d9539022b180271204ef950
Andrey <uniqcle@yandex.ru>
2024-01-18 13:45:38 +0700

    use hook

---
+++ src/services/MarvelServiceHook.js
@@ -0,0 +1,37 @@
+import { useHttp } from "../hooks/http.hook";
+
+const useMarvelServiceHook = () => {
+  const { loading, request, error, clearError } = useHttp();
+
+  const _apiBase = "https://gateway.marvel.com:443/v1/public/";
+  const _apiKey = "apikey=98a6b2e2b70899f4d1c9cf64df5fd810";
+  const _baseOffset = 210;
+
+  const getAllCharacters = async (offset = _baseOffset) => {
+    const res = await request(
+      `${_apiBase}characters?limit=9&offset=${offset}&${_apiKey}`
+    );
+    return res.data.results.map(_transformCharacter);
+  };
+
+  const getCharacter = async (id) => {
+    const res = await request(`${_apiBase}characters/${id}?${_apiKey}`);
+    return _transformCharacter(res.data.results[0]);
+  };
+
+  const _transformCharacter = (char) => {
+    return {
+      id: char.id,
+      name: char.name,
+      description: char.description,
+      thumbnail: char.thumbnail.path + "." + char.thumbnail.extension,
+      homepage: char.urls[0].url,
+      wiki: char.urls[1].url,
+      comics: char.comics.items,
+    };
+  };
+
+  return { loading, error, clearError, getAllCharacters, getCharacter };
+};
+
+export default useMarvelServiceHook;
