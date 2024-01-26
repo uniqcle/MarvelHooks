23c5ecca8878dfaa8253cdb5744217ef1a505bae
Andrey <uniqcle@yandex.ru>
2024-01-26 12:33:06 +0700

    suspense

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -1,6 +1,9 @@
+import { lazy, Suspense } from "react";
 import { Routes, Route } from "react-router-dom";
-import { MainPage, ComicsPage, Page404, SingleComicPage } from "../pages";
+import { MainPage, ComicsPage, SingleComicPage } from "../pages";
 import AppHeader from "../appHeader/AppHeader";
+
+const Page404 = lazy(() => import("../pages/404"));
 
 const App = () => {
   return (
@@ -8,12 +11,14 @@ const App = () => {
       <div className="app">
         <AppHeader />
         <main>
-          <Routes>
-            <Route path="/" element={<MainPage />} />
-            <Route path="comics" element={<ComicsPage />} />
-            <Route path="comics/:comicId" element={<SingleComicPage />} />
-            <Route path="*" element={<Page404 />} />
-          </Routes>
+          <Suspense fallback={<span>Loading suspense...</span>}>
+            <Routes>
+              <Route path="/" element={<MainPage />} />
+              <Route path="comics" element={<ComicsPage />} />
+              <Route path="comics/:comicId" element={<SingleComicPage />} />
+              <Route path="*" element={<Page404 />} />
+            </Routes>
+          </Suspense>
         </main>
       </div>
     </>
